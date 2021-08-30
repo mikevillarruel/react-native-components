12bf2d3ef7b3506b2e5535e39f2203b7e40ed0f9
Michael Villarruel <alejovillarruel@gmail.com>
2021-08-29 20:42:04 -0500

    Section List

---
+++ src/screens/CustomSectionListScreen.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { SectionList, Text, View } from 'react-native';
+import { HeaderTitle } from '../components/HeaderTitle';
+import { ItemSeparator } from '../components/ItemSeparator';
+import { styles } from '../theme/appTheme';
+
+interface Casas {
+    casa: string;
+    data: string[];
+}
+
+const casas: Casas[] = [
+    {
+        casa: "DC Comics",
+        data: ["Batman", "Superman", "Robin", "Batman", "Superman", "Robin", "Batman", "Superman", "Robin", "Batman", "Superman", "Robin", "Batman", "Superman", "Robin", "Batman", "Superman", "Robin"]
+    },
+    {
+        casa: "Marvel Comics",
+        data: ["Antman", "Thor", "Spiderman", "Antman", "Batman", "Superman", "Robin", "Batman", "Superman", "Robin", "Batman", "Superman", "Robin", "Batman", "Superman", "Robin", "Batman", "Superman", "Robin", "Batman", "Superman", "Robin"]
+    },
+    {
+        casa: "Anime",
+        data: ["Kenshin", "Goku", "Saitama", "Batman", "Superman", "Robin", "Batman", "Superman", "Robin", "Batman", "Superman", "Robin"]
+    }
+];
+
+export const CustomSectionListScreen = () => {
+    return (
+        <View style={{ ...styles.globalMargin, flex: 1 }}>
+            <SectionList
+                sections={casas}
+                keyExtractor={(item, index) => item + index}
+
+                ListHeaderComponent={() => <HeaderTitle title="Section List" />}
+                ListFooterComponent={() => (
+                    <View style={{ marginBottom: 50 }}>
+                        <HeaderTitle title={`Houses Total: ${casas.length}`} />
+                    </View>
+                )}
+
+                renderItem={({ item }) => <Text>{item}</Text>}
+
+                renderSectionHeader={({ section }) => (
+                    <View style={{ backgroundColor: 'white' }}>
+                        <HeaderTitle title={section.casa} />
+                    </View>
+                )}
+                renderSectionFooter={({ section }) => <HeaderTitle title={`Total: ${section.data.length}`} />}
+
+                SectionSeparatorComponent={() => <ItemSeparator />}
+                // ItemSeparatorComponent={()=><ItemSeparator/>}
+
+                stickySectionHeadersEnabled={true}
+                showsVerticalScrollIndicator={false}
+
+            />
+        </View>
+    )
+}
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,18 +4,9 @@ import { FlatListMenuItem } from '../components/FlatListMenuItem';
 import { menuItems } from '../data/menuItems';
 import { styles } from '../theme/appTheme';
 import { HeaderTitle } from '../components/HeaderTitle';
+import { ItemSeparator } from '../components/ItemSeparator';
 
 export const HomeScreen = () => {
-
-    const itemSeparator = () => {
-        return (
-            <View style={{
-                borderBottomWidth: 1,
-                opacity: 0.4,
-                marginVertical: 8,
-            }}></View>
-        )
-    }
 
     return (
         <View style={{
@@ -27,7 +18,7 @@ export const HomeScreen = () => {
                 renderItem={({ item }) => <FlatListMenuItem menuItem={item} />}
                 keyExtractor={(item) => item.name}
                 ListHeaderComponent={() => <HeaderTitle title='Menu Options' />}
-                ItemSeparatorComponent={itemSeparator}
+                ItemSeparatorComponent={() => <ItemSeparator />}
             />
         </View>
     )
